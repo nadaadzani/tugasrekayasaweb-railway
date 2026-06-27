10ac25b8bb705dadf13ae3d81222cf6da6870078
Nada Adzani <adzaninada@gmail.com>
2026-06-27 19:43:58 +0700

    db seed fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,8 +23,8 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('password'),
         ]);
 
-        // $this->call([
-        //     ProjectSeeder::class
-        // ]);
+        $this->call([
+            ProjectSeeder::class
+        ]);
     }
 }
